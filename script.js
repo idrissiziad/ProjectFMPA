76ef87cf5fbdaf031d737897c7c5eb2ebe868af9
idrissiziad <110735036+idrissiziad@users.noreply.github.com>
2025-03-03 14:58:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const quizData = [
   {
+    module: "Cardiologie", // Nouveau !
+    question: "Quelle est la fonction du ventricule gauche ?",
+    options: ["...", "...", "..."],
+    correctAnswer: 2
+  },
+  {
+    module: "Neurologie", // Nouveau !
+    question: "Qu'est-ce qu'un neurone ?",
+    options: ["...", "...", "..."],
+    correctAnswer: 0
+  }
+];
+const quizData = [
+  {
     question: "2 + 2 = ?",
     options: ["3", "4", "5"],
     correct: 1
